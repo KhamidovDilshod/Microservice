097ae8189859fe2660e0c0bf1d18fd49639c23a2
KhamidovDilshod <khamidovdilshodbek@gmail.com>
2022-09-06 07:38:18 +0300

    Added AutoMapper package to gRPC project

--- Discount/Discount.gRPC/Discount.gRPC/Services/DiscountService.cs
+++ Discount/Discount.gRPC/Discount.gRPC/Services/DiscountService.cs
@@ -1,8 +1,30 @@
 ﻿using Discount.gRPC.pros;
+using Discount.gRPC.Repositories;
+using Grpc.Core;
 
 namespace Discount.gRPC.Services;
 
 public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
 {
-    
+    private readonly IDiscountRepository _discountRepository;
+    private readonly ILogger<DiscountService> _logger;
+
+    public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+    {
+        _discountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _discountRepository.GetDiscount(request.ProductName);
+
+        if (coupon == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound,
+                $"Discount with ProductName={request.ProductName} not found"));
+        }
+
+        var couponModel = _mapper.Map<CouponModel>(coupon);
+    }
 }
